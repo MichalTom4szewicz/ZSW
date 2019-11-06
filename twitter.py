ded58139e8b50ad45ee751c065f8ca4729c9fc90
Wojciech Czarnecki <48015260+s235714@users.noreply.github.com>
2019-11-06 17:04:16 +0100

    Add files via upload

--- twitter.py
+++ twitter.py
@@ -1,5 +1,10 @@
 import tweepy
 import Adafruit_DHT
+import sounddevice as sd
+from scipy.io.wavfile import write
+from time import gmtime, strftime
+from scipy.io import wavfile as wav
+import os
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("Y509knpZaOHKERLudDQ5yCa1a",
@@ -8,7 +13,57 @@ auth.set_access_token("1182367262453518336-C50nxYQTZwKZrmLuHleyorIvSVUO95",
                       "wvRMz5fqiHGhS6JOQqXDt5l40cw5yrA1IBfJNZV3vQHYg")
 
 def get_sound():
-        # here
+	timestamp = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
+
+	ts = str(timestamp)+".wav"
+	print(ts)
+
+
+	fs = 44100  # Sample rate
+	seconds = 5  # Duration of recording
+
+
+	myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+	sd.wait()  # Wait until recording is finished
+	write(ts, fs, myrecording)  # Save as WAV file
+
+	rate, data = wav.read(ts)
+
+	min = 0
+	max = 0
+
+	print(int(data[10][0]))
+	for i in data:
+	    for j in i:
+	        a = float(j)
+	        if a < min:
+	            min = a
+	        if a > max:
+	            max = a
+
+	print("minimum: " + str(min))
+	print("maximum: " + str(max))
+
+	list = os.listdir(r".")
+
+	nazwa = "dane.txt"
+
+
+	#jakiestam przetwarzanie ocena tego czy jest glosno czy cicho, najlepiej w dB
+	# moze cos takiego https://github.com/SuperShinyEyes/spl-meter-with-RPi
+
+	if nazwa in list:
+	    f = open("dane.txt", "a")
+	    f.write(str(timestamp) + " " + str(min) + " " + str(max)+"\n")
+	    f.close()
+	else:
+	    f = open("dane.txt", "x")
+	    f.write(str(timestamp) + " " + str(min) + " " + str(max)+"\n")
+	    f.close()
+
+	os.remove(ts)
+	return (min+max)/2
+
 
 
 def auth_twitter():
