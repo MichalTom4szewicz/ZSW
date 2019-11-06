54799e4fc957882238ac777d8d5e5534cdaa2b9b
KBialecki <36888024+KBialecki@users.noreply.github.com>
2019-11-06 17:35:22 +0100

    Add files via upload

--- twitter.py
+++ twitter.py
@@ -12,58 +12,57 @@ auth = tweepy.OAuthHandler("Y509knpZaOHKERLudDQ5yCa1a",
 auth.set_access_token("1182367262453518336-C50nxYQTZwKZrmLuHleyorIvSVUO95",
                       "wvRMz5fqiHGhS6JOQqXDt5l40cw5yrA1IBfJNZV3vQHYg")
 
+
 def get_sound():
-	timestamp = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
+    timestamp = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
 
-	ts = str(timestamp)+".wav"
-	print(ts)
+    ts = str(timestamp) + ".wav"
+    print(ts)
 
+    fs = 44100  # Sample rate
+    seconds = 5  # Duration of recording
 
-	fs = 44100  # Sample rate
-	seconds = 5  # Duration of recording
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
+    write(ts, fs, myrecording)  # Save as WAV file
 
+    rate, data = wav.read(ts)
 
-	myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-	sd.wait()  # Wait until recording is finished
-	write(ts, fs, myrecording)  # Save as WAV file
+    min = 0
+    max = 0
 
-	rate, data = wav.read(ts)
+    print(int(data[10][0]))
+    for i in data:
+        for j in i:
+            a = float(j)
+            if a < min:
+                min = a
+            if a > max:
+                max = a
 
-	min = 0
-	max = 0
+    print("minimum: " + str(min))
+    print("maximum: " + str(max))
 
-	print(int(data[10][0]))
-	for i in data:
-	    for j in i:
-	        a = float(j)
-	        if a < min:
-	            min = a
-	        if a > max:
-	            max = a
-
-	print("minimum: " + str(min))
-	print("maximum: " + str(max))
-
-	list = os.listdir(r".")
-
-	nazwa = "dane.txt"
+    os.remove(ts)
+    return min, max, timestamp
 
 
-	#jakiestam przetwarzanie ocena tego czy jest glosno czy cicho, najlepiej w dB
-	# moze cos takiego https://github.com/SuperShinyEyes/spl-meter-with-RPi
+def to_file(min, max, timestamp):
+    list = os.listdir(r".")
 
-	if nazwa in list:
-	    f = open("dane.txt", "a")
-	    f.write(str(timestamp) + " " + str(min) + " " + str(max)+"\n")
-	    f.close()
-	else:
-	    f = open("dane.txt", "x")
-	    f.write(str(timestamp) + " " + str(min) + " " + str(max)+"\n")
-	    f.close()
+    nazwa = "dane.txt"
 
-	os.remove(ts)
-	return (min+max)/2
+    # jakiestam przetwarzanie ocena tego czy jest glosno czy cicho, najlepiej w dB
+    # moze cos takiego https://github.com/SuperShinyEyes/spl-meter-with-RPi
 
+    if nazwa in list:
+        f = open("dane.txt", "a")
+        f.write(str(timestamp) + " " + str(min) + " " + str(max) + "\n")
+        f.close()
+    else:
+        f = open("dane.txt", "x")
+        f.write(str(timestamp) + " " + str(min) + " " + str(max) + "\n")
+        f.close()
 
 
 def auth_twitter():
@@ -87,8 +86,9 @@ def init_twitter():
 
 def post_update(api, sensor_data):
     message_body = "Today's temperature: " + str(sensor_data["temperature"]) + "°C\n" \
-                   "Humidity: " + str(sensor_data["humidity"]) + "%\n" \
-                   "Loudness: " + str(sensor_data["loudness"])  # TODO: get sensor data
+                                                                               "Humidity: " + str(
+        sensor_data["humidity"]) + "%\n" \
+                                   "Loudness: " + str(sensor_data["loudness"])  # TODO: get sensor data
     api.update_status(message_body)
     print("Status uploaded!")
 
@@ -104,7 +104,10 @@ def get_humi():
 
 
 if __name__ == "__main__":
-    sensor_data = {"temperature": get_temp(), "humidity": get_humi(), "loudness": get_sound()}
+    min, max, timestamp = get_sound()
+    sensor_data = {"temperature": get_temp(), "humidity": get_humi(), "loudness": int((min+max)/2)}
+    to_file(min, max, timestamp)
+
     auth_twitter()
     api = init_twitter()
     post_update(api, sensor_data)
