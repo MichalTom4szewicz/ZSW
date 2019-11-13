58da7c1c7b44c3bd943d2d12a0fde3d78173c9e7
Wojciech Czarnecki <48015260+s235714@users.noreply.github.com>
2019-11-13 16:38:39 +0100

    Add files via upload

--- twitter.py
+++ twitter.py
@@ -1,3 +1,13 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import urllib.request
+import os.path
+import random
+from PIL import Image, ImageFilter, ImageFont, ImageDraw
+import PIL
+import datetime
+
 import tweepy
 import Adafruit_DHT
 import sounddevice as sd
@@ -6,6 +16,9 @@ from time import gmtime, strftime
 from scipy.io import wavfile as wav
 import os
 import time
+
+
+
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("Y509knpZaOHKERLudDQ5yCa1a",
                            "WOpjxR4FFAuEXgTvZ5gSqPBWAkgem8r82XyKnyV4kfAjOPxvh0")
@@ -47,7 +60,7 @@ def get_sound():
     return min, max, timestamp
 
 
-def to_file(min, max, timestamp):
+def to_file(min, max, timestamp, sensors):
     list = os.listdir(r".")
 
     nazwa = "dane.txt"
@@ -57,11 +70,13 @@ def to_file(min, max, timestamp):
 
     if nazwa in list:
         f = open("dane.txt", "a")
-        f.write(str(timestamp) + " " + str(min) + " " + str(max) + "\n")
+        f.write(str(timestamp) + " " + str(min) + " " + str(max) + " " + str(sensors["temperature"]) \
+            + " " + str(sensors["humidity"]) + " " + str(sensors["loudness"]) + "\n")
         f.close()
     else:
         f = open("dane.txt", "x")
-        f.write(str(timestamp) + " " + str(min) + " " + str(max) + "\n")
+        f.write(str(timestamp) + " " + str(min) + " " + str(max) + " " + str(sensors["temperature"]) \
+            + " " + str(sensors["humidity"]) + " " + str(sensors["loudness"]) + "\n")
         f.close()
 
 
@@ -88,8 +103,8 @@ def post_update(api, sensor_data):
     message_body = "Today's temperature: " + str(sensor_data["temperature"]) + "°C\n" \
                                                                                "Humidity: " + str(
         sensor_data["humidity"]) + "%\n" \
-                                   "Loudness: " + str(sensor_data["loudness"])  # TODO: get sensor data
-    api.update_status(message_body)
+                                   "Loudness: " + str(sensor_data["loudness"])
+    api.update_with_media("nowe.jpg", message_body)
     print("Status uploaded!")
 
 
@@ -103,8 +118,114 @@ def get_humi():
     return humidity
 
 
-if __name__ == "__main__":
+def make_img(sensory):
+    html = urlopen('https://www.flickr.com/search/?q=flowers')
+    bs = BeautifulSoup(html, 'html.parser')
 
+    images = re.findall("[a-z \- A-Z 0-9 \. ]+\.com[a-z \- A-Z 0-9 \. \/ \_ \-]+\.jpg", str(bs))
+
+    licznik = 1
+
+    for image in images:
+        urllib.request.urlretrieve('https://' + str(image), 'img' + str(licznik) + '.jpg')
+        print('pyk')
+        licznik = licznik + 1
+
+    print('pobrano')
+    licznik -= 1
+    list = os.listdir(r".")
+
+    list.remove('obraz.py')
+
+    wybor = random.randrange(1, 25)
+
+    file_name = list[wybor]
+
+    # ------------------------------
+
+    im = Image.open(file_name)
+    x, y = im.size
+
+    mnoznik = 2
+    new_x = x * mnoznik
+    new_y = y * mnoznik
+    im = im.resize((new_x, new_y), PIL.Image.NEAREST)
+
+    im = im.filter(ImageFilter.GaussianBlur(4))
+
+    pixels = im.load()
+
+    for i in range(im.size[0]):
+        for j in range(im.size[1]):
+            r, g, b = pixels[i, j]
+            r += 60
+            g += 60
+            b += 60
+            if (r > 255):
+                r = 255
+            if (b > 255):
+                b = 255
+            if (g > 255):
+                g = 255
+            pixels[i, j] = (r, g, b)
+
+    color = (0, 0, 255)
+
+    fnt = ImageFont.truetype('Comic Sans MS.ttf', 40)
+
+    d = ImageDraw.Draw(im)
+
+    row1 = "WITAMY"
+    row2 = "SERDECZNIE"
+
+    w1, h1 = d.textsize(row1, font=fnt)
+    w2, h2 = d.textsize(row2, font=fnt)
+
+    print(str(w1))
+    print(str(w2))
+
+    print(str(new_x))
+
+    d.text(((new_x / 2) - (w1 / 2), (new_y / 2 - h1)), row1, font=fnt, fill=color)
+    d.text(((new_x / 2) - (w2 / 2), (new_y / 2 + h1)), row2, font=fnt, fill=color)
+
+    off1 = 60
+    off2 = 150
+
+    kolor_tla = (random.randrange(1, 255), random.randrange(1, 255), random.randrange(1, 255))
+
+    img = Image.new('RGB', (new_x, off2), color=kolor_tla)
+
+    img1 = Image.new('RGB', (new_x, off1), color=kolor_tla)
+
+    total_width = new_x
+    max_height = new_y + off1 + off2
+
+    new_im = Image.new('RGB', (total_width, max_height))
+
+    images = [img1, im, img]
+
+    x_offset = 0
+    for i in images:
+        new_im.paste(i, (0, x_offset))
+        x_offset += i.size[1]
+
+    temp = str(sensory["temperature"])
+    wilgonosc = str(sensory["humidity"])
+    halas = str(sensory["loudness"])
+
+    kolor_font = (255 - kolor_tla[0], 255 - kolor_tla[1], 255 - kolor_tla[2])
+
+    d1 = ImageDraw.Draw(new_im)
+    d1.text((10, off1 + new_y), "temperatura: " + temp + "°C", font=fnt, fill=kolor_font)
+    d1.text((10, off1 + new_y + 45), "wilgotnosc: " + wilgonosc + "%", font=fnt, fill=kolor_font)
+    d1.text((10, off1 + new_y + 90), "halas: " + halas, font=fnt, fill=kolor_font)
+    d1.text((10, 0), "Dzis jest: " + str(datetime.date.today()), font=fnt, fill=kolor_font)
+
+    new_im.save("nowe.jpg")
+
+
+if __name__ == "__main__":
 
     auth_twitter()
     api = init_twitter()
@@ -112,6 +233,7 @@ if __name__ == "__main__":
     while True:
         time.sleep(3600)
         min, max, timestamp = get_sound()
-        sensor_data = {"temperature": get_temp(), "humidity": get_humi(), "loudness": int((min+max)/2)}
-        to_file(min, max, timestamp)
+        sensor_data = {"temperature": get_temp(), "humidity": get_humi(), "loudness": float((min+max)/2)}
+        to_file(min, max, timestamp, sensor_data)
+        make_img(sensor_data)
         post_update(api, sensor_data)
